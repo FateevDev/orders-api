8409a8283a7a1ff614d821c1928d299a7eed448c
vasiliy <dev.fateev@gmail.com>
2025-09-19 19:16:51 +0500

    feat(repository): improve error handling in RedisRepository
    Enhance `RedisRepository` methods to use specific error types for not-found scenarios and improve error message clarity. The update includes introducing `ErrOrderWithIdNotFound` and `ErrOrdersNotFound`, replacing generic errors to provide more descriptive responses and better error resolution in downstream services.

--- repository/order/redis.go
+++ repository/order/redis.go
@@ -18,6 +18,13 @@ func orderIdKey(id uint64) string {
 	return fmt.Sprintf("order:%d", id)
 }
 
+func ErrOrderWithIdNotFound(id uint64) error {
+	return fmt.Errorf("order %d not found", id)
+}
+func ErrOrdersNotFound() error {
+	return errors.New("orders not found")
+}
+
 func (r *RedisRepository) Insert(ctx context.Context, order model.Order) error {
 	marshal, err := json.Marshal(order)
 
@@ -26,24 +33,20 @@ func (r *RedisRepository) Insert(ctx context.Context, order model.Order) error {
 	}
 
 	key := orderIdKey(order.OrderID)
-	res := r.Client.SetNX(ctx, key, marshal, 0)
+	err = r.Client.SetNX(ctx, key, marshal, 0).Err()
 
-	if err := res.Err(); err != nil {
+	if err != nil {
 		return fmt.Errorf("failed to insert order: %w", err)
 	}
 
 	return nil
 }
 
-func ErrNotFound(id uint64) error {
-	return fmt.Errorf("order %d not found", id)
-}
-
 func (r *RedisRepository) FindById(ctx context.Context, id uint64) (model.Order, error) {
 	value, err := r.Client.Get(ctx, orderIdKey(id)).Result()
 
 	if errors.Is(err, redis.Nil) {
-		return model.Order{}, ErrNotFound(id)
+		return model.Order{}, ErrOrderWithIdNotFound(id)
 	} else if err != nil {
 		return model.Order{}, fmt.Errorf("failed to get order: %w", err)
 	}
@@ -61,7 +64,9 @@ func (r *RedisRepository) FindById(ctx context.Context, id uint64) (model.Order,
 func (r *RedisRepository) List(ctx context.Context) ([]model.Order, error) {
 	value, err := r.Client.GetRange(ctx, "order", 0, 100).Result()
 
-	if err != nil {
+	if errors.Is(err, redis.Nil) {
+		return nil, ErrOrdersNotFound()
+	} else if err != nil {
 		return nil, fmt.Errorf("failed to get orders: %w", err)
 	}
 
@@ -79,7 +84,7 @@ func (r *RedisRepository) Delete(ctx context.Context, id uint64) error {
 	err := r.Client.Del(ctx, orderIdKey(id)).Err()
 
 	if errors.Is(err, redis.Nil) {
-		return ErrNotFound(id)
+		return ErrOrderWithIdNotFound(id)
 	} else if err != nil {
 		return fmt.Errorf("failed to delete order: %w", err)
 	}
@@ -88,9 +93,17 @@ func (r *RedisRepository) Delete(ctx context.Context, id uint64) error {
 }
 
 func (r *RedisRepository) Update(ctx context.Context, id uint64, order model.Order) error {
-	set := r.Client.Set(ctx, orderIdKey(id), order, 0)
+	marshal, err := json.Marshal(order)
 
-	if err := set.Err(); err != nil {
+	if err != nil {
+		return fmt.Errorf("failed to marshal order: %w", err)
+	}
+
+	err = r.Client.SetXX(ctx, orderIdKey(id), marshal, 0).Err()
+
+	if errors.Is(err, redis.Nil) {
+		return ErrOrderWithIdNotFound(id)
+	} else if err != nil {
 		return fmt.Errorf("failed to update order: %w", err)
 	}
 
