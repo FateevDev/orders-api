691e21a104d0ef3896c663ee17c6ae61bd374ea8
vasiliy <dev.fateev@gmail.com>
2025-09-20 15:52:22 +0500

    feat(app): integrate order repository with handler
    Refactor app initialization to set up order repository in the `Order` handler. Updated routing logic to depend on the app context, enabling a more cohesive structure and proper resource management. Includes enhanced error handling for order creation.

--- handler/order.go
+++ handler/order.go
@@ -1,16 +1,62 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
+	"math/rand/v2"
 	"net/http"
+	"time"
 
-	"github.com/FateevDev/orders-api/repository/order"
+	"github.com/FateevDev/orders-api/model"
+	orderRepo "github.com/FateevDev/orders-api/repository/order"
+	"github.com/google/uuid"
 )
 
-type Order struct{}
+type Order struct {
+	Repository *orderRepo.RedisRepository
+}
 
 func (o *Order) Create(w http.ResponseWriter, r *http.Request) {
-	order.RedisRepository{}
+	var body struct {
+		CustomerID uuid.UUID        `json:"customer_id"`
+		LineItems  []model.LineItem `json:"line_items"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	now := time.Now().UTC()
+
+	order := model.Order{
+		OrderID:    rand.Uint64(),
+		CustomerID: body.CustomerID,
+		LineItems:  body.LineItems,
+		CreatedAt:  &now,
+	}
+
+	err := o.Repository.Insert(r.Context(), order)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	marshal, err := json.Marshal(order)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	_, err = w.Write(marshal)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func (o *Order) List(w http.ResponseWriter, r *http.Request) {
